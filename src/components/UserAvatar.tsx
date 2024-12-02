0e29653c8a35e3773e1c00110632090a14f95a85
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-02 21:09:13 +0530

    Create a component for User Avatar

---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { auth } from "../../auth";
+import Image from "next/image";
+
+const UserAvatar = async () => {
+  const session = await auth();
+  const user = session?.user;
+  console.log(session);
+  return (
+    <div className="flex items-center gap-1">
+      <p>{user?.name}</p>
+      <Image
+        src={user?.image!}
+        alt={`${user?.name} profile-image`}
+        height={38}
+        width={38}
+        className="rounded-full"
+      />
+    </div>
+  );
+};
+
+export default UserAvatar;
