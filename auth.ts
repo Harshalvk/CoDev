031291a744818c6ac058a5d343254bdf26ef390e
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-02 21:11:54 +0530

    ADD: GitHub login support & custom login page route

--- auth.ts
+++ auth.ts
@@ -3,8 +3,12 @@ import { db } from "@/db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import type { Adapter } from "next-auth/adapters";
 import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db) as Adapter,
-  providers: [Google],
+  providers: [Google, GitHub],
+  pages: {
+    signIn: "/login",
+  },
 });
