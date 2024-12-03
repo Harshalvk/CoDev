cb86ae81cf2ab4179143f19cd08a1b5fe59ab6d0
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-03 14:54:23 +0530

    FIX: configure auth.js to verify user is present in the DB or not

--- auth.ts
+++ auth.ts
@@ -1,14 +1,59 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { db } from "@/db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import type { Adapter } from "next-auth/adapters";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
 
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db) as Adapter,
+  session: {
+    strategy: "jwt",
+  },
   providers: [Google, GitHub],
   pages: {
     signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user, account }) {
+      const dbUser = await db.query.users.findFirst({
+        where: (users, { eq }) => eq(users.email, token.email!),
+      });
+
+      if (!dbUser) {
+        throw new Error("No user with email found");
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+        emailVerified: dbUser.emailVerified,
+      };
+    },
+
+    async session({ token, session }) {
+      if (token && token.id && token.name && token.email && token.picture) {
+        session.user = {
+          id: token.id as string,
+          name: token.name,
+          email: token.email,
+          image: token.picture,
+        };
+      } else {
+        throw new Error("Invalid token data");
+      }
+
+      return session;
+    },
   },
 });
