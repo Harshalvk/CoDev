9efb3ca8662632060b71ad0b156ce88b76285511
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-05 00:22:23 +0530

    ADD: Install and Setup Nextjs-Toploader package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "../components/ThemeProvider";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,6 +36,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <NextTopLoader color="#2299DD" />
           {children}
         </ThemeProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <>
       <Header />
+      <main className="min-h-screen p-24">
+        <div className="flex justify-between items-center w-full">
+          <h1 className="text-4xl font-semibold tracking-tighter">
+            Find Dev Rooms
+          </h1>
+          <Button asChild>
+            <Link href={"/create-room"}>Create Room</Link>
+          </Button>
+        </div>
+      </main>
     </>
   );
 }
