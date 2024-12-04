936ff8780c38a1d87640ca5511b5614c4d35e081
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-04 23:45:14 +0530

    Complete Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,31 @@
 import React from "react";
-import UserAvatar from "./UserAvatar";
 import { ModeToggle } from "./ModeToggle";
+import { auth } from "../../auth";
+import UserAccountNav from "./UserAccountNav";
+import SignInButton from "./SignIn";
 
-const Header = () => {
+const Header = async () => {
+  const session = await auth();
+  if (!session?.user) {
+    return null;
+  }
+
   return (
-    <div className="mt-4 max-w-7xl flex items-center justify-between mx-auto py-4 px-6 border rounded-full">
-      <div>
-        <span className="text-2xl font-bold tracking-tighter">CoDev</span>
-      </div>
-      <div className="flex gap-2">
-        <UserAvatar />
-        <ModeToggle />
+    <div className="container mx-auto">
+      <div className="mt-4 mx-3 max-w-7xl flex items-center justify-between py-4 px-6 border rounded-full">
+        <div>
+          <span className="text-2xl font-bold tracking-tighter">CoDev</span>
+        </div>
+        <div className="flex gap-2 items-center">
+          <ModeToggle />
+          <div className="flex items-center">
+            {session.user ? (
+              <UserAccountNav user={session.user} />
+            ) : (
+              <SignInButton text="SignIn" />
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
