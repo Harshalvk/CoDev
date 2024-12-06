e871695daf0beb10c1fb6f40643250e3e94b04e1
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-06 20:35:47 +0530

    ADD: logic to redirect user when not login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,35 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import RoomCard from "@/components/RoomCard";
 import { getRooms } from "@/data-access/rooms";
+import { auth } from "../../auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const rooms = await getRooms();
+  const session = await auth();
 
-  return (
-    <>
-      <Header />
-      <main className="min-h-screen py-24">
-        <div className="flex justify-between items-center w-full">
-          <h1 className="text-4xl font-semibold tracking-tighter">
-            Find Dev Rooms
-          </h1>
-          <Button asChild>
-            <Link href={"/create-room"}>Create Room</Link>
-          </Button>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap-2 mt-5">
-          {rooms.map((room) => (
-            <RoomCard room={room} key={room.id} />
-          ))}
-        </div>
-      </main>
-    </>
-  );
+  if (!session?.user) {
+    return redirect("/login");
+  } else {
+    return (
+      <>
+        <Header />
+        <main className="min-h-screen py-24">
+          <div className="flex justify-between items-center w-full">
+            <h1 className="text-4xl font-semibold tracking-tighter">
+              Find Dev Rooms
+            </h1>
+            <Button asChild>
+              <Link href={"/create-room"}>Create Room</Link>
+            </Button>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap-2 mt-5">
+            {rooms.map((room) => (
+              <RoomCard room={room} key={room.id} />
+            ))}
+          </div>
+        </main>
+      </>
+    );
+  }
 }
