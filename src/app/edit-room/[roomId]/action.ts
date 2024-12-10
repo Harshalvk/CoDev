f7c998f193e93c5b6ee340c7a629ce290e737de4
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-10 22:02:11 +0530

    ADD: edit room route

---
+++ src/app/edit-room/[roomId]/action.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { Room } from "@/db/schema";
+import { auth } from "../../../../auth";
+import { revalidatePath } from "next/cache";
+import { getRoom, updateRoomData } from "@/data-access/rooms";
+import { redirect } from "next/navigation";
+
+export async function updateRoomAction(roomData: Omit<Room, "userId">) {
+  const session = await auth();
+
+  if (!session) {
+    throw new Error("You must be logged in to create a room.");
+  }
+
+  const room = await getRoom(roomData.id);
+
+  if (room?.userId !== session.user.id) {
+    throw new Error("User not authorized!");
+  }
+
+  await updateRoomData(roomData);
+
+  revalidatePath("/my-rooms");
+  revalidatePath(`/edit-rooms/${roomData.id}`);
+  redirect("/my-rooms");
+}
